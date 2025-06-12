eab40cfe70f25c32038f3e357825e882ca268361
dr-akoto <quintonnana123@gmail.com>
2025-06-12 17:17:36 +0100

    started image preprocesing from ABIDE

---
+++ image_prepocessing.py
@@ -0,0 +1,25 @@
+import numpy as np
+import nibabel as nib
+import cv2
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.preprocessing.image import img_to_array
+import tensorflow as tf
+import pandas as pd
+import os
+
+# Load the ABIDE phenotypic CSV file
+df = pd.read_csv("Phenotypic_V1_0b.csv")
+
+# Show the first few rows
+print(df[['SITE_ID', 'SUB_ID', 'DX_GROUP', 'AGE_AT_SCAN']].head())
+
+asd_subjects= df[df['DX_GROUP'] == 1]
+print (asd_subjects[['SITE_ID', 'SUB_ID']].head())
+
+print("Environment setup complete.")
+# def load_nifti_image(file_path):
+#     """Load a NIfTI image file."""
+#     img = nib.load(file_path)
+#     return img.get_fdata()
